826f526717764d7188b1b0e32b858a30dfdac0af
Rob van der Heijden <413117@student.fontys.nl>
2019-02-07 10:11:49 +0100

    Test commit teamleider class

---
+++ ProftaakS2/Classes/Teamleider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProftaakS2.Classes
+{
+    public class Teamleider
+    {
+        private string voornaam;
+        private string achternaam;
+        private int leeftijd;
+    }
+}
